3d030a1a03ae2076bc9b10a1cda2793888209932
Chris Evans <chris@streamgo.co.uk>
2019-10-25 18:58:01 +0100

    WIP

---
+++ src/Functions/Delete.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Iter\Functions;
+
+use Iter\Types\Iter;
+
+/**
+ * Trait Delete
+ * @package Iter\Functions
+ * @mixin Iter
+ */
+trait Delete
+{
+    public function delete($value): self
+    {
+        $out = [];
+        $deleted = false;
+        foreach ($this->arr as $v) {
+            if ($deleted || $v !== $value) {
+                $out[] = $v;
+            } else {
+                $deleted = true;
+            }
+        }
+        return $out;
+    }
+}
--- tests/IntsTest.php
+++ tests/IntsTest.php
@@ -102,6 +102,30 @@ class IntsTest extends MainTestCase
                     ],
                 ],
             ],
+            'delete' => [
+                'run' => function (array $in, array $ex, int $del) {
+                    $this->assertEquals($ex, Ints::with($in)->delete($del)->get());
+                },
+                'cases' => [
+                    [
+                        'in' => [],
+                        'ex' => [],
+                        'del' => 5,
+                    ], [
+                        'in' => [1, 2, 3],
+                        'ex' => [1, 2, 3],
+                        'del' => 5,
+                    ], [
+                        'in' => [1, 2, 3],
+                        'ex' => [1, 3],
+                        'del' => 2,
+                    ], [
+                        'in' => [1, 2, 3, 2, 1],
+                        'ex' => [1, 3, 2, 1],
+                        'del' => 2,
+                    ],
+                ],
+            ],
             'map' => [
                 'run' => function (array $in, array $ex, ?callable $fn) {
                     $this->assertEquals($ex, Ints::with($in)->map($fn)->get());
